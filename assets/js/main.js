6d1c21427db2a75b8611e31d0064d870e2179145
nevesiya <81850783+nevesiya@users.noreply.github.com>
2023-03-31 23:33:16 +0300

    Added the Intersection Observer API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 "use strict"
 
+// Menu burger
 function openMenu() {
     humbNav.classList.toggle("nav__list-box--active");
     humbBtn.classList.toggle("nav__humb-close");
@@ -18,3 +19,29 @@ function openMenu() {
 
 humbBtn.addEventListener("click", openMenu);
 
+
+
+// Animation when scrolling pages
+const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            setTimeout(() => {
+                entry.target.classList.add("anim-active");
+            }, 100);
+        } else if (entry.target.classList.contains("anim-repeat")) {
+            entry.target.classList.remove("anim-active");
+        }
+    })
+};
+
+const options = {
+    threshold: 0.5
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+const targets = document.querySelectorAll(".anim-item");
+targets.forEach((target) => {
+    console.log(target);
+    observer.observe(target);
+})
